f8fcdd2856224a8b5e9714d7e272c1acec136ca5
ShibaniLikhite <slikhite@ucsd.edu>
2023-07-23 11:55:57 -0700

    downsampling with reverse entity

--- GCN-BERT/bert_models/train_vgcn_bert.py
+++ GCN-BERT/bert_models/train_vgcn_bert.py
@@ -31,17 +31,17 @@ device = torch.device("cuda:0" if cuda_yes else "cpu")
 
 class VGCN_BERT:
     def __init__(self, model, cleanData, buildGraph, initial_predictions=None, final_predictions=None, del_stop_words=False, model_type='VGCN_BERT', train_epochs=15,
-                 dropout=0.2, batch_size=4, gcn_embedding_dim=16, learning_rate0= 1e-5, l2_decay=0.001):
-        file_path = "/sbksvol/shibani/logs_bio-bert-large-final.txt"
+                 dropout=0.2, batch_size=8, gcn_embedding_dim=16, learning_rate0= 1e-5, l2_decay=0.001):
+        file_path = "/sbksvol/shibani/logs_down_sample_reverse.txt"
         file = open(file_path, "w")
         self.model = model
         self.data = cleanData
         self.graph = buildGraph
         print("device",device,file=file)
-        MAX_SEQ_LENGTH = 200 + gcn_embedding_dim
+        MAX_SEQ_LENGTH = 300 + gcn_embedding_dim
         gradient_accumulation_steps = 1
         bert_model_scale = 'bert-base-uncased'
-        bert_state_path = '/sbksvol/jiawei/re-model-data/weights/biobert_large_v1.1_pubmed_torch'
+        bert_state_path = '/sbksvol/jiawei/re-model-data/weights/biobert_base_v1.0_torch'
         do_lower_case = True
         warmup_proportion = 0.1
         perform_metrics_str = ['weighted avg', 'f1-score']
@@ -232,7 +232,10 @@ class VGCN_BERT:
             print('Epoch : %d, %s: %.3f Acc : %.3f on %s, Spend:%.3f minutes for evaluation'
                   % (epoch_th, ' '.join(perform_metrics_str), 100 * f1_metrics, 100. * ev_acc, dataset_name,
                      (end - start) / 60.0),file=file)
-            print('f1_micro: %d, std dev: %d' %(f1_metrics_micro,std_dev), file=file)
+            print('f1_micro: %d' %(f1_metrics_micro), file=file)
+            print('std_dev: %d' %(std_dev), file=file)
+            print('f1_micro: %d' %(f1_metrics_micro))
+            print('std_dev: %d' %(std_dev))
             print('--------------------------------------------------------------',file=file)
             return ev_loss, ev_acc, f1_metrics
 
@@ -329,7 +332,7 @@ class VGCN_BERT:
         plt.legend()
 
         # Save the figure
-        plt.savefig('/sbksvol/shibani/bio-bert-large-final.png')           
+        plt.savefig('/sbksvol/shibani/down_sample_reverse.png')           
         print('\n**Optimization Finished!,Total spend:', (time.time() - train_start) / 60.0,file = file)
         pred, confidence = predict(model, test_dataloader)
 
